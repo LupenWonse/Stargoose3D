9c2d4f24e1fe2d43e79f1dfce9dc2e57903ded34
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-01-26 10:45:08 -0500

    Imported music manager from Overturn
    Basic music manager implementing Singleton pattern for playing music

---
+++ Assets/Controllers/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	private static MusicManager singleMusicManager;
+
+	// Using Load function to use a singleton pattern
+	void Awake () {
+		print ("Awake");
+		if (singleMusicManager == null) {
+			print ("singleton");
+			DontDestroyOnLoad (this.gameObject);
+			singleMusicManager = this;
+		} else {
+			GameObject.Destroy (this.gameObject);
+		}
+	}
+
+	void Start () {
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
