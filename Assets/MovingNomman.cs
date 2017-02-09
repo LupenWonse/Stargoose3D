b6c8e981361cc4a74ecb22349929d14021709660
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-02-09 17:07:39 -0500

    Implemented moving enemy
    From now on enemies are called Noomans!

---
+++ Assets/MovingNomman.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingNomman : BadGuysController {
+
+	public Vector3 movementVector;
+
+	private new Rigidbody rigidbody;
+
+	void Awake(){
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		float currentYSpeed = rigidbody.velocity.y;
+		rigidbody.velocity = movementVector + new Vector3 (0, Mathf.Min (currentYSpeed, 0), 0);
+	}
+
+	void LateUpdate ()
+	{
+		// Hack to freeze rotation
+		Vector3 currentRotation = rigidbody.rotation.eulerAngles;
+		currentRotation.y = 0;
+		rigidbody.rotation = Quaternion.Euler(currentRotation);
+	}
+
+
+}
