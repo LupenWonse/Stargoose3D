bf9dd89c02cb75a691461ddab04d1186e0f79cb1
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-03-27 10:29:24 -0400

    Collectible objects

---
+++ Assets/Collectible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+	public enum COLLECTIBLE {AMMO, FUEL, SHIELD}
+	public COLLECTIBLE type;
+
+	public void OnTriggerEnter(Collider collider){
+		// If we hit the player
+
+		// Player collects the item
+		
+		// Destroy the collectible object
+	}
+}
